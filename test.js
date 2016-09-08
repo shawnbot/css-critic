4b3b91d218da6dc888dfeeea584c249445551f51
Shawn Allen <shawn.allen@gsa.gov>
2016-09-08 15:41:16 -0700

    url in argv

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 'use strict';
 const critical = require('./');
+const url = process.argv[3] || 'http://localhost:4000';
 
-critical('http://localhost:4000', {diff: true})
+critical(url, {diff: true})
   .then(
     styles => console.log(styles.join('\n')),
     error => console.error(error)
